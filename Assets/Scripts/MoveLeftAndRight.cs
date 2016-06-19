4631c300e4da003dc8165158280c8d2e77399baf
Ryan Mysliwiec <retroshadegit@gmail.com>
2016-06-19 13:13:00 -0400

    Initial Commit

---
+++ Assets/Scripts/MoveLeftAndRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftAndRight : MonoBehaviour {
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//This moves object left or right based on input
+		transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;	
+			//GetAxis smooths the stopping of the object; GetAxisRaw immediately stops object upon release;
+
+	}
+}
